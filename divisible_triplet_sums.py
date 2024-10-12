8ccb8b1056e1c66c0c08cda76624489e53400edc
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2024-10-12 20:35:08 +0400

    some more problems

---
+++ divisible_triplet_sums.py
@@ -0,0 +1,8 @@
+def divisible_triplets(nums: list[int], d: int) -> int:
+    lo, hi = 0, len(nums) - 1
+    for num in nums:
+        ...
+
+
+if __name__ == "__main__":
+    assert divisible_triplets([[3, 3, 4, 7, 8]], 5) == 3
---
+++ regex.py
@@ -0,0 +1,19 @@
+from itertools import zip_longest
+
+
+def regex_matcher_only_dot(inp: str, regex: str) -> bool:
+    for i, pat in zip_longest(inp, regex):
+        if not i or not pat or pat != "." and i != pat:
+            return False
+    return True
+
+
+def regex_matcher(inp: str, regex: str) -> bool:
+    pass
+
+
+if __name__ == "__main__":
+    assert regex_matcher_only_dot("ray", "ra.") is True
+    assert regex_matcher_only_dot("raymond", "ra.") is False
+    assert regex_matcher("chat", ".*at") is True
+    assert regex_matcher("chats", ".*at") is False
